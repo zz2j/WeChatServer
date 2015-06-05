2208749fff12f2169c5c5d1b39e1c58e727d2800
zz2j <vangogh0@163.com>
2015-06-05 10:34:03 +0800

    怎么还丢文件了呢？

---
+++ Wechat.Server/Controllers/HomeController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Web.Mvc;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.Entities.Request;
+
+using Senparc.Weixin.MP.MessageHandlers;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Helpers;
+using Senparc.Weixin.MP.MvcExtension;
+using System.IO;
+using Wechat.Server.Wx.Handler;
+using System.Diagnostics;
+namespace Wechat.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static readonly string token = "wechat.zz2j";
+        public static readonly string encodingAesKey = "casco.signal";
+        public static readonly string appId = "wx4df221a4e9845aea";
+
+        //Bind Server
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult CheckSign(PostModel postModel, string echostr)
+        {
+            if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
+            {
+                return Content(echostr);
+            }
+            else
+            {
+                Trace.TraceInformation("url绑定");
+                return Content("failed:" + postModel.Signature + "," + CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, token) +
+                    "。如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+            }
+        }
+
+        //Proxy Message
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Post(PostModel postModel)
+        {
+            if (!CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
+            {
+                Trace.TraceError("参数错误");
+                return Content("参数错误");
+            }
+            Trace.TraceInformation("Post验证成功，进入微信处理过程");
+            postModel.Token = token;
+            postModel.EncodingAESKey = encodingAesKey;
+            postModel.AppId = appId;
+
+            //v4.2.2之后的版本，可以设置每个人上下文消息储存的最大数量，防止内存占用过多，如果该参数小于等于0，则不限制
+            var maxRecordCount = 10;
+
+            var logPath = Server.MapPath(string.Format("~/App_Data/MP/{0}/", DateTime.Now.ToString("yyyy-MM-dd")));
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            //自定义MessageHandler，对微信请求的详细判断操作都在这里面。
+            var messageHandler = new VehicleMessageHandler(Request.InputStream, postModel, maxRecordCount);
+            try
+            {
+
+                //测试时可开启此记录，帮助跟踪数据，使用前请确保App_Data文件夹存在，且有读写权限。
+                messageHandler.RequestDocument.Save(Path.Combine(logPath, string.Format("{0}_Request_{1}.txt", DateTime.Now.Ticks, messageHandler.RequestMessage.FromUserName)));
+                if (messageHandler.UsingEcryptMessage)
+                {
+                    messageHandler.EcryptRequestDocument.Save(Path.Combine(logPath, string.Format("{0}_Request_Ecrypt_{1}.txt", DateTime.Now.Ticks, messageHandler.RequestMessage.FromUserName)));
+                }
+
+                /* 如果需要添加消息去重功能，只需打开OmitRepeatedMessage功能，SDK会自动处理。
+                 * 收到重复消息通常是因为微信服务器没有及时收到响应，会持续发送2-5条不等的相同内容的RequestMessage*/
+                messageHandler.OmitRepeatedMessage = true;
+
+
+                //执行微信处理过程
+                messageHandler.Execute();
+
+                //测试时可开启，帮助跟踪数据
+
+                //if (messageHandler.ResponseDocument == null)
+                //{
+                //    throw new Exception(messageHandler.RequestDocument.ToString());
+                //}
+
+                if (messageHandler.ResponseDocument != null)
+                {
+                    messageHandler.ResponseDocument.Save(Path.Combine(logPath, string.Format("{0}_Response_{1}.txt", DateTime.Now.Ticks, messageHandler.RequestMessage.FromUserName)));
+                }
+
+                if (messageHandler.UsingEcryptMessage)
+                {
+                    //记录加密后的响应信息
+                    messageHandler.FinalResponseDocument.Save(Path.Combine(logPath, string.Format("{0}_Response_Final_{1}.txt", DateTime.Now.Ticks, messageHandler.RequestMessage.FromUserName)));
+                }
+
+                //return Content(messageHandler.ResponseDocument.ToString());//v0.7-
+                return new FixWeixinBugWeixinResult(messageHandler);//为了解决官方微信5.0软件换行bug暂时添加的方法，平时用下面一个方法即可
+                //return new WeixinResult(messageHandler);//v0.8+
+            }
+            catch (Exception ex)
+            {
+                using (TextWriter tw = new StreamWriter(Server.MapPath("~/App_Data/Error_" + DateTime.Now.Ticks + ".txt")))
+                {
+                    tw.WriteLine("ExecptionMessage:" + ex.Message);
+                    tw.WriteLine(ex.Source);
+                    tw.WriteLine(ex.StackTrace);
+                    //tw.WriteLine("InnerExecptionMessage:" + ex.InnerException.Message);
+
+                    if (messageHandler.ResponseDocument != null)
+                    {
+                        tw.WriteLine(messageHandler.ResponseDocument.ToString());
+                    }
+
+                    if (ex.InnerException != null)
+                    {
+                        tw.WriteLine("========= InnerException ==========");
+                        tw.WriteLine(ex.InnerException.Message);
+                        tw.WriteLine(ex.InnerException.Source);
+                        tw.WriteLine(ex.InnerException.StackTrace);
+                    }
+
+                    tw.Flush();
+                    tw.Close();
+                }
+                return Content("");
+            }
+        }
+
+    }
+}
