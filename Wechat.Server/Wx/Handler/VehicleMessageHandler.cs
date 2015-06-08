0d16f6b49a31f743998027a46162bb69e37fa10a
zz2j <vangogh0@163.com>
2015-06-08 11:08:54 +0800

    修复bug 6.8.1

--- Wechat.Server/Wx/Handler/VehicleMessageHandler.cs
+++ Wechat.Server/Wx/Handler/VehicleMessageHandler.cs
@@ -164,7 +164,7 @@ namespace Wechat.Server.Wx.Handler
                     webUrl, requestMessage.FromUserName, enText.ToString(), timestamp.ToString(), nonce);
 
                 string text = string.Format("欢迎使用卡斯柯微信服务号，绑定账号请访问下面的地址：\r\n\r\n <a href=\"{0}\">点击这里</a>",loginUrl);
-                enhancedResponseMessage.Content = text +"\r\n" + " ";
+                enhancedResponseMessage.Content = text +"\r\n" + loginUrl;
                 return enhancedResponseMessage;
             }
             return base.OnEvent_ClickRequest(requestMessage);
